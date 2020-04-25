7e55199366c9e8e0664ae3fb5125c461470e91b7
Kenneth <kennethzhang374@gmail.com>
2020-04-25 00:28:26 -0700

    quick review of routing

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express(); // add methods to app
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello From the server side!');
+  res
+    .status(200)
+    .json({ message: 'Hello From the server side!', app: 'Natours' });
 });
 
+app.post('/', (req, res) => {
+  res.send(`You can post to this endpoint...`);
+});
 const PORT = 3008;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
