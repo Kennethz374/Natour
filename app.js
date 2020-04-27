9b61ea232287a91a54652cd81ac74ca997bb1b23
Kenneth <kennethzhang374@gmail.com>
2020-04-26 23:10:51 -0700

    create a get route, read file on top and send back

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express(); // add methods to app
 
-app.get('/', (req, res) => {
+app.use(express.json()); //need this middleware to access the "req.body"
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello From the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('you can post to this endpoint');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
-    .json({ message: 'Hello From the server side!', app: 'Natours' });
+    .json({ status: 'success', result: tours.length, data: { tours: tours } });
 });
 
-app.post('/', (req, res) => {
-  res.send(`You can post to this endpoint...`);
+app.post(`/api/v1/tours`, (req, res) => {
+  // console.log(req.body);
+
+  res.send(`Done`);
 });
 const PORT = 3008;
 app.listen(PORT, () => {
