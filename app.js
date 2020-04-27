91d86d62d4480370c11c26b754772c736044103c
Kenneth <kennethzhang374@gmail.com>
2020-04-26 23:22:34 -0700

    write new tour to file and send back that tour to console

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ app.post(`/api/v1/tours`, (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTours);
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, tours, err => {
+    res.status(201).json({
+      status: 'success',
+      result: tours.length,
+      data: { tour: newTour }
+    });
+  });
   res.send(`Done`);
 });
 const PORT = 3008;
