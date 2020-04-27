99f250ff9788d198fdf2405f0e063b768e8ed957
Kenneth <kennethzhang374@gmail.com>
2020-04-26 23:14:11 -0700

    store new id, last item id + 1

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post(`/api/v1/tours`, (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
   res.send(`Done`);
 });
 const PORT = 3008;
