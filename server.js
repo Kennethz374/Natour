086ee873f398663f489c8bfeef56ef2a1bb3ba36
Kenneth <kennethzhang374@gmail.com>
2020-05-03 23:27:32 -0700

    setting eslint plugin

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-console.log(app.get('env')); // this will allow you to see what env you in
+dotenv.config({ path: './config.env' });
+
+// console.log(app.get('env')); // this will allow you to see what env you in
 
 const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
