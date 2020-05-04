a12827298f3818e46df64c4dde95407a318ab7e9
Kenneth <kennethzhang374@gmail.com>
2020-05-03 22:23:44 -0700

    make a checkID handler to check id before hiting routes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,16 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (Number(val > tours.length)) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID'
+    });
+  }
+  next();
+}; //this middleware helps to check ID before hiting the route. keep route just for route.
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -17,12 +27,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = Number(req.params.id);
   const tour = tours.find(el => el.id === id);
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid ID'
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: { tour }
@@ -49,12 +53,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (Number(req.params.id > tours.length)) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid ID'
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -64,12 +62,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (Number(req.params.id > tours.length)) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid ID'
-    });
-  }
   res.status(204).json({
     status: 'success',
     data: null
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,10 +10,7 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-  next();
-});
+router.param('id', tourController.checkID);
 //middleware for only the tour route with id
 
 router
