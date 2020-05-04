3d530d6ea74d91c3f5140600294e6834552466ea
Kenneth <kennethzhang374@gmail.com>
2020-05-03 22:12:41 -0700

    route.param to get id

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,12 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
+router.param('id', (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  next();
+});
+//middleware for only the tour route with id
+
 router
   .route('/')
   .get(tourController.getAllTours)
