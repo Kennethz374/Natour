57a3e67428d71620ef712c38a67d890f641c7f7e
Kenneth <kennethzhang374@gmail.com>
2020-05-06 00:08:07 -0700

    add mongodb

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const morgan = require('morgan');
 const tourRouter = require(`./routes/tourRoutes`);
 const userRouter = require(`./routes/userRoutes`);
 
-const app = express();
+const app = express(); // add methods to app
 
+// 1) middleware
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev')); 
-app.use(express.json()); 
-
-
+  app.use(morgan('dev')); // logging req ex: GET /api/v1/tours 200 19.412 ms - 8681
+}
+app.use(express.json()); //need this middleware to access the "req.body"
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
+//3) routes
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
