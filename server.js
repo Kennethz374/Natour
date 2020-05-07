abfc6e950e2a4de0d463ca587d3775725c8efab4
Kenneth <kennethzhang374@gmail.com>
2020-05-06 23:38:43 -0700

    delete comment and continue mongodb

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// console.log(app.get('env')); // this will allow you to see what env you in
-
 const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
-//learning mongodb basic today!!
