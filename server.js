efd11f042209693f3ef01ac07408687d72e38c2c
Kenneth <kennethzhang374@gmail.com>
2020-05-07 23:34:12 -0700

    succesfully connected to mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
 
 const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
