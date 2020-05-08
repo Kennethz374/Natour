10c1e6dc5c63a901cfcf612980488ca3fd49f0eb
Kenneth <kennethzhang374@gmail.com>
2020-05-07 23:43:48 -0700

    specify a schema and create a tour

--- server.js
+++ server.js
@@ -13,11 +13,23 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: true
+    useFindAndModify: false
   })
   .then(() => {
     console.log('DB connection successful');
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  }, //schema type options
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, 'A tour must have a price'] }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
