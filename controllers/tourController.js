f9947c36cf1049c196b14d308ba14b9a828d4a84
Kenneth <kennethzhang374@gmail.com>
2020-05-18 23:02:57 -0700

    minor changes on delete

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -61,28 +61,24 @@ exports.updateTour = async (req, res) => {
       }
     });
   } catch (err) {
-    res.status(200).json({
+    res.status(404).json({
       status: 'fail',
-      data: {
-        tour: 'Updated tour here.......'
-      }
+      message: err
     });
   }
 };
 
 exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndRemove(req.params.id);
-    res.status(200).json({
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
       status: 'success',
       data: null
     });
   } catch (err) {
-    res.status(200).json({
+    res.status(404).json({
       status: 'fail',
-      data: {
-        tour: 'Updated tour here.......'
-      }
+      message: err
     });
   }
 };
