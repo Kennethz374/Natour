24daaba72197e28264954052e1b166e1d435883f
Kenneth <kennethzhang374@gmail.com>
2020-05-18 22:31:16 -0700

    refactor Tour controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,29 +1,5 @@
 const Tour = require('../models/tourModel');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-// exports.checkID = (req, res, next, val) => {
-//   // if (Number(val > tours.length)) {
-//   //   return res.status(404).json({
-//   //     status: 'fail',
-//   //     message: 'invalid ID'
-//   //   });
-//   // }
-//   next();
-// };
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'invalid request'
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -43,11 +19,20 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success'
-    // data: { tour: newTour }
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'faill',
+      message: 'Invalid data sent!'
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
