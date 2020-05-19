ff3ee8c7dfbbc128d13eb9b324e4dee34b90cba2
Kenneth <kennethzhang374@gmail.com>
2020-05-18 22:50:51 -0700

    complete CRUD refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -19,6 +19,7 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({_id: req.params.id})
     res.status(200).json({
       status: 'success',
       data: { tour }
@@ -47,18 +48,41 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'Updated tour here.......'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(200).json({
+      status: 'fail',
+      data: {
+        tour: 'Updated tour here.......'
+      }
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndRemove(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(200).json({
+      status: 'fail',
+      data: {
+        tour: 'Updated tour here.......'
+      }
+    });
+  }
 };
